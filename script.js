08760b0325eef988755a580fb320bd17ef19dac2
ah3863 <ah3863@nyu.edu>
2025-04-03 14:33:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ $(document).ready(function() {
       $(this).prop("disabled", true); // Disable button after it's clicked
     });
   });
+
+git init
+git add .
+git commit -m "Initial commit"
+git branch -M main
+git remote add origin https://github.com/ah3863/AdvancedGISAssignment2.git
+git push -u origin main
